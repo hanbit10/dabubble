9ed30cff5226dad35d0d681d4d150fe38bd8b681
hanbit10 <chanbit10@gmail.com>
2024-08-28 22:44:30 +0200

    update components

---
+++ src/app/components/direct-chat/direct-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-direct-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './direct-chat.component.html',
+  styleUrl: './direct-chat.component.scss'
+})
+export class DirectChatComponent {
+
+}
