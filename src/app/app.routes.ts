29e95e1c8bd8e66791b8c5aea9a3918a026a199d
hanbit10 <chanbit10@gmail.com>
2024-08-29 10:23:35 +0200

    add font nunito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,5 +11,6 @@ export const routes: Routes = [
   {
     path: 'main',
     component: MainComponent,
+    children: [],
   },
 ];
--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  Router,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
+export class MainComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
