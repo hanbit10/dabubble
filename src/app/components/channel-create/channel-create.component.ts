ebe83e041c5cebf573c7387866f8978312ac994b
hanbit10 <chanbit10@gmail.com>
2024-08-29 13:48:42 +0200

    update channel create

--- src/app/components/channel-create/channel-create.component.ts
+++ src/app/components/channel-create/channel-create.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-channel-create',
   standalone: true,
   imports: [],
   templateUrl: './channel-create.component.html',
-  styleUrl: './channel-create.component.scss'
+  styleUrl: './channel-create.component.scss',
 })
-export class ChannelCreateComponent {
+export class ChannelCreateComponent implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.closeCard();
+    }
+  }
+
+  closeCard() {
+    const cardClose = document.getElementById('card-close');
+    cardClose?.addEventListener('click', () => {
+      const createChannel = document.getElementById('channel-create');
+      createChannel?.classList.add('hidden');
+    });
+  }
+  close() {
+    this.resetCard();
+  }
+
+  resetCard() {}
 }
