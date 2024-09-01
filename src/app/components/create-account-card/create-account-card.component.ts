07feada6fc9ede02ef09a3b2c7e1a6f621f34f4b
Jörg Habermann <joerg1985@gmx.net>
2024-09-01 18:28:10 +0200

    refactored log in components

---
+++ src/app/components/create-account-card/create-account-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-account-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-account-card.component.html',
+  styleUrl: './create-account-card.component.scss'
+})
+export class CreateAccountCardComponent {
+
+}
--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
+import { LogInCardComponent } from '../../components/log-in-card/log-in-card.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink,MatInputModule],
+  imports: [RouterLink,MatInputModule,LogInCardComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
