2239f022617149fe254fb24d48f390d112234f01
hanbit10 <chanbit10@gmail.com>
2024-09-06 11:46:27 +0200

    update channel profile

--- src/app/components/channel-chat/channel-profile/channel-profile.component.ts
+++ src/app/components/channel-chat/channel-profile/channel-profile.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { Channel } from '../../../models/channels';
+import { UserService } from '../../../services/user.service';
+import { UserProfile } from '../../../models/users';
 
 @Component({
   selector: 'app-channel-profile',
   standalone: true,
   imports: [],
   templateUrl: './channel-profile.component.html',
-  styleUrl: './channel-profile.component.scss'
+  styleUrl: './channel-profile.component.scss',
 })
-export class ChannelProfileComponent {
+export class ChannelProfileComponent implements OnInit {
+  private usersSubscription!: Subscription;
+  private _items = new BehaviorSubject<Channel>({} as Channel);
+  @Input() set getCurrentChannel(value: Channel) {
+    this._items.next(value);
+  }
 
+  filteredUsers: UserProfile[] = [];
+
+  get currentChannel(): Channel {
+    return this._items.getValue();
+  }
+  constructor(public userService: UserService) {}
+  ngOnInit(): void {
+    this._items.subscribe((currChannel) => {
+      if (currChannel) {
+        this.usersSubscription = this.userService.users$.subscribe((users) => {
+          if (users) {
+            this.filteredUsers = users.filter((user) => {
+              return (currChannel.usersIds || []).includes(user.uid);
+            });
+          }
+        });
+      }
+    });
+  }
 }
