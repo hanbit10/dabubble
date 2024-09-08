300eaaaf88b8baeed05aa31642b8840d4ef95879
Jörg Habermann <joerg1985@gmx.net>
2024-09-08 18:28:22 +0200

    google auth

--- src/app/components/log-in-card/log-in-card.component.ts
+++ src/app/components/log-in-card/log-in-card.component.ts
@@ -5,20 +5,22 @@ import { LoginCreateAccountService } from '../../services/login-create-account.s
 import { FormsModule } from '@angular/forms';
 import { UserProfile } from '../../models/users';
 import { Router } from '@angular/router';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 @Component({
   selector: 'app-log-in-card',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, ],
   templateUrl: './log-in-card.component.html',
   styleUrl: './log-in-card.component.scss',
 })
 export class LogInCardComponent {
+  googleAuth = new GoogleAuthProvider();
   users: UserProfile[] = [];
   constructor(
     public logService: LoginCreateAccountService,
     public dataBase: UserService,
-    private router: Router
+    private router: Router    
   ) {
     this.loadUsers();
   }
@@ -34,17 +36,56 @@ export class LogInCardComponent {
   userLogin() {
     let user: UserProfile;
     let userIndex: number;
-    userIndex = this.users.findIndex(
-      (i) => i.email === this.logService.loginMail
-    );
-    user = this.users[userIndex];
-    console.log(user);
-    if (this.logService.loginPassword === user.password) {
-      this.router.navigate([`/main/${user.uid}`]);
+    if (this.logService.loginMail && this.logService.loginPassword) {
+      userIndex = this.users.findIndex(
+        (i) => i.email === this.logService.loginMail
+      );
+      user = this.users[userIndex];
+      console.log(user);
+      if (this.logService.loginPassword === user.password) {
+        this.router.navigate([`/main/${user.uid}`]);
+      }
     }
   }
 
   guestLogin() {
     this.router.navigate([`/main/guest`]);
   }
+
+  googleLogin() {
+    const auth = getAuth();
+
+  signInWithPopup(auth, this.googleAuth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    if (credential) {
+      const token = credential.accessToken;
+    }
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+    this.router.navigate([`/main/${user.uid}`]);
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });  
+  }
+
+ 
+
+  setGoogleUser() {
+
+  }
+
 }
+
+
