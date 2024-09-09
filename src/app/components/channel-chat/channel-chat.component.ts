61d068c8c881c712a2f83ff6ace219f33a0d9bb7
hanbit10 <chanbit10@gmail.com>
2024-09-09 22:34:49 +0200

    update channel profile

--- src/app/components/channel-chat/channel-chat.component.ts
+++ src/app/components/channel-chat/channel-chat.component.ts
@@ -25,7 +25,6 @@ export class ChannelChatComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe((paramMap) => {
       const id = paramMap.get('id');
-      console.log('This is paramMap', paramMap);
       if (id) {
         this.channelId = id;
         console.log('channel id', this.channelId);
