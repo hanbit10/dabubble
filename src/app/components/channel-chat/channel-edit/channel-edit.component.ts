c8d0d45a1828af2b4388850a0a05169070e29f9d
hanbit10 <chanbit10@gmail.com>
2024-09-13 12:33:13 +0200

    update edit

---
+++ src/app/components/channel-chat/channel-edit/channel-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './channel-edit.component.html',
+  styleUrl: './channel-edit.component.scss'
+})
+export class ChannelEditComponent {
+
+}
