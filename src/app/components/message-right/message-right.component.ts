c1fe022c9e77bc92e072c73516cae008620f8fba
saskiarichter <richter_saskia@gmx.net>
2024-09-14 14:17:49 +0200

    organize scss

--- src/app/components/message-right/message-right.component.ts
+++ src/app/components/message-right/message-right.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ProfileService } from '../../services/profile.service';
+import { ChannelService } from '../../services/channel.service';
 
 @Component({
   selector: 'app-message-right',
@@ -12,7 +13,7 @@ export class MessageRightComponent {
   @Input()userName = '';
   settingIsOpen: boolean = false;
 
-  constructor(public profileService: ProfileService){}
+  constructor(public profileService: ProfileService, public channelService: ChannelService){}
 
   openProfile(){
     this.profileService.openMainProfile();
@@ -31,6 +32,6 @@ export class MessageRightComponent {
   }
 
   editMessage(){
-    
+
   }
 }
