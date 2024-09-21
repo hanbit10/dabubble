7402f0e85ea4b25e66cd16263c179335a7eb1337
hanbit10 <chanbit10@gmail.com>
2024-09-21 22:11:22 +0200

    update thread service

--- src/app/services/thread.service.ts
+++ src/app/services/thread.service.ts
@@ -1,12 +1,44 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {
+  collection,
+  Firestore,
+  onSnapshot,
+  Timestamp,
+} from '@angular/fire/firestore';
+import { Message } from '../models/message';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThreadService {
+  firestore: Firestore = inject(Firestore);
+  private threadSubject = new BehaviorSubject<any[]>([]);
   threadIsOpen: boolean = false;
+  threads: any[] = [];
   constructor() {}
 
+  get threads$() {
+    return this.threadSubject.asObservable();
+  }
+
+  subThreadList(currentChannelId: string, currentMessageId: string) {
+    const docRef = collection(
+      this.firestore,
+      'channels',
+      currentChannelId,
+      'messages',
+      currentMessageId,
+      'threads'
+    );
+    return onSnapshot(docRef, (list) => {
+      this.threads = [];
+      list.forEach((doc) => {
+        this.threads.push(doc.data());
+      });
+      this.threadSubject.next(this.threads);
+    });
+  }
   openThread() {
     this.threadIsOpen = true;
   }
@@ -15,5 +47,26 @@ export class ThreadService {
     this.threadIsOpen = false;
   }
 
-  createThread() {}
+  async sendThread(
+    sentThread: any,
+    currentChannelId: string,
+    currentMessageId: string,
+    currentUserId: string
+  ) {
+    const docRef = collection(
+      this.firestore,
+      'channels',
+      currentChannelId,
+      'messages',
+      currentMessageId,
+      'threads'
+    );
+
+    let data: Message = {
+      text: sentThread.text,
+      sentBy: currentUserId,
+      sentAt: Timestamp.fromDate(new Date()),
+      uid: '',
+    };
+  }
 }
