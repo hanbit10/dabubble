6e582f06cb798af605068819e2639655ea288c52
hanbit10 <chanbit10@gmail.com>
2024-09-23 21:23:36 +0200

    update direct chat

--- src/app/components/direct-chat/direct-chat.component.ts
+++ src/app/components/direct-chat/direct-chat.component.ts
@@ -10,6 +10,8 @@ import { MessageRightComponent } from '../message-right/message-right.component'
 import { ProfileService } from '../../services/profile.service';
 import { Message } from '../../models/message';
 import { FormsModule, NgForm } from '@angular/forms';
+import { DirectChatService } from '../../services/direct-chat.service';
+import { MessageService } from '../../services/message.service';
 
 @Component({
   selector: 'app-direct-chat',
@@ -33,6 +35,8 @@ export class DirectChatComponent {
   otherUserId: string = '';
   currentUserId: string = '';
   private routeSub: Subscription = new Subscription();
+  private messageSubscription!: Subscription;
+  currentChatId: string = '';
 
   sentMessage: any = {
     text: '',
@@ -43,16 +47,46 @@ export class DirectChatComponent {
   constructor(
     public userService: UserService,
     private route: ActivatedRoute,
-    public profileService: ProfileService
+    public profileService: ProfileService,
+    public directChatService: DirectChatService,
+    public messageService: MessageService
   ) {}
 
   async ngOnInit() {
-    this.routeSub = this.route.params.subscribe((params) => {
+    this.route.params.subscribe((params) => {
       this.otherUserId = params['id'];
+      this.route.parent?.paramMap.subscribe(async (paramMap) => {
+        const id = paramMap.get('id');
+        if (id) {
+          console.log('current User id', id);
+          console.log('other user id', this.otherUserId);
+          this.currentUserId = id;
+          const exist = this.directChatService.isExistingChat(
+            this.otherUserId,
+            this.currentUserId
+          );
+          console.log((await exist) == false);
+          if ((await exist) == false) {
+            console.log('creating new chat');
+            this.currentChatId = '';
+            this.currentMessages = [];
+            this.directChatService.createNewChat(
+              this.otherUserId,
+              this.currentUserId
+            );
+          } else {
+            this.currentChatId = await this.directChatService.getChatId(
+              this.otherUserId,
+              this.currentUserId
+            );
+            this.messageService.subMessageList(this.currentChatId, 'chats');
+            console.log('current chat id', this.currentChatId);
+          }
+        }
+      });
 
       this.usersSubscription = this.userService.users$.subscribe((users) => {
         this.allUsers = users;
-
         let filteredUser = this.allUsers.find((user) => {
           return user.uid === this.otherUserId;
         });
@@ -60,26 +94,29 @@ export class DirectChatComponent {
           this.otherUser = filteredUser;
         }
       });
-    });
 
-    this.route.parent?.paramMap.subscribe((paramMap) => {
-      const id = paramMap.get('id');
-      if (id) {
-        console.log('current User id', id);
-        console.log('other user id', this.otherUserId);
-      }
+      this.messageSubscription = this.messageService.messages$.subscribe(
+        (messages) => {
+          this.currentMessages = messages.sort((a, b) => {
+            if (a.sentAt && b.sentAt) {
+              return a.sentAt.toDate().getTime() - b.sentAt.toDate().getTime();
+            }
+            return 0;
+          });
+        }
+      );
     });
   }
 
   onSubmit(messageForm: NgForm) {
-    // if (messageForm.valid) {
-    //   this.messageService.sendMessage(
-    //     this.sentMessage,
-    //     this.currentChatId,
-    //     this.currentUserId,
-    //     'chats'
-    //   );
-    // }
+    if (messageForm.valid) {
+      this.messageService.sendMessage(
+        this.sentMessage,
+        this.currentChatId,
+        this.currentUserId,
+        'chats'
+      );
+    }
   }
 
   ngOnDestroy() {
--- src/app/services/direct-chat.service.ts
+++ src/app/services/direct-chat.service.ts
@@ -1,10 +1,92 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {
+  addDoc,
+  collection,
+  Firestore,
+  getDocs,
+  query,
+  setDoc,
+  Timestamp,
+  where,
+} from '@angular/fire/firestore';
+import { DirectChat } from '../models/direct-chat';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DirectChatService {
+  firestore: Firestore = inject(Firestore);
   constructor() {}
 
-  getChatId(otherUserId: string, currentUserId: string) {}
+  async createNewChat(otherUserId: string, currentUserId: string) {
+    let data: DirectChat = {
+      usersIds: [otherUserId, currentUserId],
+      uid: '',
+      createdAt: Timestamp.fromDate(new Date()),
+    };
+
+    const docRef = collection(this.firestore, 'chats');
+    const querySnapshot = await addDoc(docRef, data);
+    const uid = querySnapshot.id;
+    await setDoc(querySnapshot, { ...data, uid });
+  }
+
+  async isExistingChat(otherUserId: string, currentUserId: string) {
+    let found = false;
+    const docRef = collection(this.firestore, 'chats');
+    const q = query(docRef, where('usersIds', 'array-contains', currentUserId));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      if (
+        otherUserId == currentUserId &&
+        doc.data()['usersIds'][0] == otherUserId &&
+        doc.data()['usersIds'][1] == currentUserId
+      ) {
+        found = true;
+        return;
+      } else if (
+        doc.data()['usersIds'][0] == otherUserId &&
+        doc.data()['usersIds'][1] == currentUserId
+      ) {
+        found = true;
+        return;
+      }
+    });
+    return found;
+  }
+
+  async getChatId(otherUserId: string, currentUserId: string) {
+    const docRef = collection(this.firestore, 'chats');
+    const q = query(docRef, where('usersIds', 'array-contains', currentUserId));
+    const querySnapshot = await getDocs(q);
+    let chatId: string = '';
+    console.log(currentUserId);
+    console.log(otherUserId);
+    querySnapshot.forEach((doc) => {
+      if (
+        otherUserId == currentUserId &&
+        doc.data()['usersIds'][0] == otherUserId &&
+        doc.data()['usersIds'][1] == currentUserId
+      ) {
+        chatId = doc.id;
+        return;
+      } else if (
+        doc.data()['usersIds'][0] == otherUserId &&
+        doc.data()['usersIds'][1] == currentUserId
+      ) {
+        chatId = doc.id;
+        return;
+      }
+    });
+    return chatId;
+  }
+
+  findMyChat(otherUserId: string, currentUserId: string, doc: any) {
+    return (
+      otherUserId == currentUserId &&
+      doc.data()['usersIds'][0] == otherUserId &&
+      doc.data()['usersIds'][1] == currentUserId
+    );
+  }
 }
